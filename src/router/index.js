82ae5138e1ce78d161e979b0bc07f4b2993e2790
walaaosamamoh <waloosama@gmail.com>
2025-08-09 19:19:35 +0300

    Add navigation guard to protect routes with firebase auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { getAuth } from 'firebase/auth'
 import LoginView from '../views/LoginView.vue'
 import SignupView from '../views/SignupView.vue'
 import ChatView from '../views/ChatView.vue'
@@ -20,8 +21,24 @@ const router = createRouter({
       path: '/chat',
       name: 'chat',
       component: ChatView,
+      meta: {
+        requiresAuth: true, // This route requires authentication
+      },
     }
   ],
+})
+
+// Navigation guard to check authentication
+router.beforeEach((to, from, next) => {
+  const auth= getAuth();
+  const user = auth.currentUser;
+
+  if(to.meta.requiresAuth && !user){
+    next('/')
+  } else {
+    // If the route does not require authentication, proceed as normal
+    next()
+  }
 })
 
 export default router
